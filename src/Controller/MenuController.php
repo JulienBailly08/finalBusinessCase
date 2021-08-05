ce01681c80c1a2137194bb1eb21f9139e791e40f
Julien_B <julienbailly08@gmail.com>
2021-08-05 19:32:55 +0200

    add dynamic navbar

---
+++ src/Controller/MenuController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+
+
+
+class MenuController extends AbstractController
+{
+    #[Route('/menu', name: 'menu')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('content/header/menu.html.twig', [
+            'categories' =>$categoryRepository->findAll(),
+        ]);
+    }
+}
