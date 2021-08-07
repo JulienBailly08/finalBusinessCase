b0bbcd80fa7f4ab1ca9b73a4ac3b9a46912d1e83
Julien_B <julienbailly08@gmail.com>
2021-08-07 17:35:24 +0200

    univers template down

--- src/Controller/UniversController.php
+++ src/Controller/UniversController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class UniversController extends AbstractController
 {
     #[Route('/univers/{id}', name: 'univers')]
-    public function index($id, ProductRepository $productRepository): Response
+    public function index($id, ProductRepository $productRepository,CategoryRepository $categoryRepository): Response
     {
+                
         return $this->render('univers/index.html.twig', [
             'products' => $productRepository->findBy(['category'=>$id]),
+            'category' => $categoryRepository->findBy(['id'=>$id]),
+            'categorySons'=>$categoryRepository->findBy(['parent'=>$id])
         ]);
     }
 }
