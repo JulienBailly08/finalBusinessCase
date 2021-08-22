76e2215f06ca1cb1df9602ee5a74d614912d5a33
Julien_B <julienbailly08@gmail.com>
2021-08-22 15:54:24 +0200

    fil Ariane fully fonctionel

--- src/Controller/UniversController.php
+++ src/Controller/UniversController.php
@@ -17,26 +17,23 @@ class UniversController extends AbstractController
         $category=$categoryRepository->findBy(['id'=>$id]);
         $categorySons=$categoryRepository->findBy(['parent'=>$id]);
         $arrayIds = [];
-        $results=[];
-
+        $results=[]; // a finaliser !! localisation de la fn ? portée des variables ?
+        
 
         function recursCat($category){   
         global $results;
         
         if($category->getParent()->getParent() != null):
-        $results[]=['name'=>$category->getParent()->getName(),'id'=>$category->getParent()->getId(),'root'=>false];
+        $results[]=['name'=>$category->getParent()->getName(),'id'=>$category->getParent()->getId(),'root'=>false,'current'=>false];
         recursCat($category->getParent());
         else :
 
-        $results[]=['name'=>$category->getParent()->getName(),'id'=>$category->getParent()->getId(),'root'=>true];  
+        $results[]=['name'=>$category->getParent()->getName(),'id'=>$category->getParent()->getId(),'root'=>true,'current'=>false];  
         endif;
 
         return $results;
-        }
-        //$results=array_reverse(recursCat($category[0]));
-        
-
-        
+        }   
+       
         // recuperation ID des produits des sous cat dans array pour effet de shuffle sur page univers vide
         if (empty($products)):
             foreach ($categorySons as $value) {
@@ -45,16 +42,19 @@ class UniversController extends AbstractController
                    $arrayIds[]=$value->getId();
                 }
             }
-        shuffle($arrayIds);    
-             
+        shuffle($arrayIds);                
         endif;
+
+        // creation tableau de données fil d'arianne et injection page active en dernière position
+        $arianeArray=array_reverse(recursCat($category[0]));
+        $arianeArray[]=['name'=>$category[0]->getName(),'id'=>$category[0]->getId(),'root'=>false,'current'=>true];
         
         return $this->render('univers/index.html.twig', [
             'products' => $products,
             'category' => $category,
             'categorySons'=>$categorySons,
             'arrayIds'=>$arrayIds,
-            'arianeArray'=>array_reverse(recursCat($category[0]))
+            'arianeArray'=>$arianeArray
         ]);
     }
 }
