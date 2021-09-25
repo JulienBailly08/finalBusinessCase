42e3cbda916e3be8e372e13fe68bf1ddebcd45fc
Julien_B <julienbailly08@gmail.com>
2021-09-25 15:36:31 +0200

    order back modif status ok and show detail too

--- src/Form/OrderBackType.php
+++ src/Form/OrderBackType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Order;
+use App\Entity\Status;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +14,11 @@ class OrderBackType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('createdAt')
-            ->add('shipment')
-            ->add('shipmentPrice')
-            ->add('delivery')
-            ->add('paymentChoice')
-            ->add('isPaid')
-            ->add('user')
-            ->add('status')
+
+            ->add('status', EntityType::class,[
+                'class'=>Status::class,
+                'choice_label'=>'information'
+            ])
         ;
     }
 
